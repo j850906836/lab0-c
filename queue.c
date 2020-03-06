34a7e25aa878f749ec6d915fd6df3238b6a3707c
j850906836 <Kaojohnnyj850906836@gmail.com>
2020-03-06 12:32:54 +0800

    Finish Queue Insert Head Part

--- queue.c
+++ queue.c
@@ -12,16 +12,23 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q == NULL) {
+        printf("No Enough Space\n");
+    } else { /* What if malloc returned NULL? */
+        q->head = NULL;
+        q->tail = NULL;
+    }
     return q;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
+    q->head = NULL;
+    q->tail = NULL;
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
+    /* Add */
     free(q);
 }
 
@@ -35,13 +42,29 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
-    return true;
+    /* Solve: What should you do if the q is NULL? */
+    if (q == NULL) {
+        printf("queue is NULL\n");
+        return false;
+    } else {
+        newh = malloc(sizeof(list_ele_t));
+        char *input;
+        size_t string_size;
+        string_size = strlen(s);
+        input = malloc(string_size);
+        int i;
+        for (i = 0; s[i] != '\0'; i++) {
+            input[i] = s[i];
+        }
+        input[i] = '\0';
+        newh->value = input;
+        newh->next = q->head;
+        if (q->head == NULL && q->tail == NULL)
+            q->head = q->tail = newh;
+        else
+            q->head = newh;
+        return true;
+    }
 }
 
 /*
@@ -56,7 +79,10 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    list_ele_t *newt;
+    newt = malloc(sizeof(list_ele_t));
+    q->tail = newt;
+    return true;
 }
 
 /*
@@ -84,7 +110,7 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    return q->size;
 }
 
 /*
